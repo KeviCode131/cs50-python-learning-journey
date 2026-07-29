ee0e569c917865846fb66bb92ab564afd9ce730e
Makatia13 <136123312+Makatia13@users.noreply.github.com>
2026-07-29 08:12:59 +0300

    Lecture 0: Practice sep and end

---
+++ python-learning-journey/Lecture-0-Functions-and-Variables/04_sep_end.py
@@ -0,0 +1,24 @@
+print("============== SEPARATORS ==============\n")
+
+first = "Kelvin"
+course = "CS50 Python"
+country = "Kenya"
+
+print("First:", first, sep="")
+print("Course:", course, sep="")
+print("Country:", country, sep="")
+
+print()
+
+print(first, "is learning", course, "from", country, sep=" ")
+
+print()
+
+print("Python", "is", "fun", sep="-")
+
+print()
+
+print("Good luck", end="... ")
+print(first + "!")
+
+print("\n========================================")
