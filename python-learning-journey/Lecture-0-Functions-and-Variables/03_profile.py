2ec2e87b02302b0af5501a96eaef19c6d31fe418
Makatia13 <136123312+Makatia13@users.noreply.github.com>
2026-07-29 08:10:12 +0300

    Lecture 0: Complete profile program

---
+++ python-learning-journey/Lecture-0-Functions-and-Variables/03_profile.py
@@ -0,0 +1,13 @@
+print("=========================== STUDENT PROFILE ===============================", end="\n\n")
+
+name = "Kelvin"
+country = "Kenya"
+course = "CS50 Python"
+
+print("Name: ", name, sep="")
+print("Country: ", country, sep="")
+print("Course: ", course, sep="")
+
+print(name, "is learning", course, "from", country, end=".\n\n")
+
+print("===========================================================", end="\n\n")
