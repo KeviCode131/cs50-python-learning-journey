ade558b81204bdfb8aa6ca8e2dd5c36a04769615
Makatia13 <136123312+Makatia13@users.noreply.github.com>
2026-07-29 07:58:49 +0300

    Lecture 0: Add variables practice

---
+++ python-learning-journey/Lecture-0-Functions-and-Variables/02_variables.py
@@ -0,0 +1,11 @@
+name = "Kelvin"
+course = "CS50 Python"
+country = "Kenya"
+
+print("Name: ", name, sep="")
+print("Course: ", course, sep="")
+print("Country: ", country, sep="")
+
+print()
+
+print(name, "is learning", course, "from", country + ".")
