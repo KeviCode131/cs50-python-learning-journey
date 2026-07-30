307d34447dba38006a5fc4865a0a6ee35206ac46
Makatia13 <136123312+Makatia13@users.noreply.github.com>
2026-07-30 09:46:49 +0300

    correction1

--- python-learning-journey/Lecture-0-Functions-and-Variables/05_int.py
+++ python-learning-journey/Lecture-0-Functions-and-Variables/05_int.py
@@ -1,5 +1,7 @@
-name = input("Name: ")
+name = "Kelvin"
+print("Name: ", name, end="\n\n")
 price = float(input("Meal Price:$ "))
 tax = float(input("Tax:$ ")) 
 
+print(end="\n\n") 
 print("Total: ", price + tax)
