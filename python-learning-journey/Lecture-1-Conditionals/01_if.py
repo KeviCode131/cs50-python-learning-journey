e6aceb0f49cfaf1dc442e02a893b2d5d882f3425
Makatia13 <136123312+Makatia13@users.noreply.github.com>
2026-08-17 06:40:40 +0300

    even_if

--- python-learning-journey/Lecture-1-Conditionals/01_if.py
+++ python-learning-journey/Lecture-1-Conditionals/01_if.py
@@ -1,9 +1,3 @@
-x = int(input("What's x? "))
-y = int(input("What's y? "))
-
-if x < y:
-    print("x is less than y")
-if x > y:
-    print("x is greater than y")
-if x == y:
-    print("x is equal to y")
+number = int(input("Enter a integer: "))
+if number % 2 == 0:
+  print("The number is even.")
